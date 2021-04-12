133ce94f5e43d3cf4f5e77cc0f1baf5dae2326dd
SUBHAM PRASAD PANDA <79370554+Subham1901@users.noreply.github.com>
2021-04-12 16:17:50 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Example{
+    public void find(int n,int a[],int index){
+        for(int i =index; i < a.length -1; i++){
+            a[i] = a[i + 1];
+          }
+        for(int i=0;i<n-1;i++){
+            System.out.print(a[i]+" ");
+        }     
+    }
+        public static void main(String arg[]) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        int a[]=new int[n];
+        for(int i=0;i<a.length;i++){
+            a[i]=sc.nextInt();
+        }
+        int index=sc.nextInt();
+            Example e=new Example();
+            e.find(n, a, index);
+    }
+}
